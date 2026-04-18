cb72e22c6c5b22e792fa3c5a478d0dc06e21c81c
BANSHEE\elinorz <elinor.zalogin@comm-it.com>
2026-04-18 18:17:51 +0300

    chnage text again

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            About
+            About me
           </a>
         </div>
       </main>
